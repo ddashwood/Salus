2c1c95689682df1d577155758c9182a2e021fe25
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2023-03-11 13:42:32 +0000

    Commenting

--- Salus/SalusSourceDbSetAttribute.cs
+++ Salus/SalusSourceDbSetAttribute.cs
@@ -7,5 +7,10 @@
 [AttributeUsage(AttributeTargets.Property)]
 public  class SalusSourceDbSetAttribute : Attribute
 {
+    /// <summary>
+    /// The name which is used for pairing source and destination entity types with each other.
+    /// If omitted, Salus will look for a destination entity type with the same class name as
+    /// the source.
+    /// </summary>
     public string? SalusName { get; set; }
 }
